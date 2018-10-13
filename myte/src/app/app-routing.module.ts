972279cb3873bfcd5fd1cd1c023a46c072bf0dbd
junyi <collau@hotmail.com>
2018-10-13 11:37:50 +0800

    Moved Slider to home page, set up nav bar and routes

---
+++ myte/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  // {
+  //   path: 'result',
+  //   component: Component1Component
+  // }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
